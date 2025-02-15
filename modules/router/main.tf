e8bfd1cf90eddc1341d738d75b4f389c80c5dfb7
Shalom Daniel <shalomd498@gmail.com>
2025-02-14 20:10:23 -0500

    readme and nat gateway tf changes

--- modules/router/main.tf
+++ modules/router/main.tf
@@ -4,3 +4,13 @@ resource "google_compute_router" "router" {
   region  = var.region
   project = var.project_id
 }
+
+# Create NAT Gateway
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.env_name}-nat"
+  project                            = var.project_id
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
